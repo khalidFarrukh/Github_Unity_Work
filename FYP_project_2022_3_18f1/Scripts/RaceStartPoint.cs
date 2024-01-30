fe572203b5e267a96b99314e9f7b25bf91e73847
Farrukh Khalid <khalidfarrukh951@gmail.com>
2024-01-31 04:04:48 +0500

    v31.1.2024.4.04.am

--- FYP_project_2022_3_18f1/Scripts/RaceStartPoint.cs
+++ FYP_project_2022_3_18f1/Scripts/RaceStartPoint.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class RaceStartPoint : MonoBehaviour
 {
-    [SerializeField]
-    private Transform start_point;
+    [SerializeField] private Transform track1_start_point;
+    [SerializeField] private Transform track2_start_point;
+    [SerializeField] private Transform track3_start_point;
+    [SerializeField] private Transform track4_start_point;
+    [SerializeField] private Transform track5_start_point;
+
+
     // Start is called before the first frame update
-    private void Start()
+    private void Awake() 
     {
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.position = start_point.position;
-        rb.rotation = start_point.rotation;
+        if(GlobalCycleTrack.TrackType==1)
+        {
+            rb.position = track1_start_point.position;
+            rb.rotation = track1_start_point.rotation;
+        }
+        if (GlobalCycleTrack.TrackType == 2)
+        {
+            rb.position = track2_start_point.position;
+            rb.rotation = track2_start_point.rotation;
+        }
+        if (GlobalCycleTrack.TrackType == 3)
+        {
+            rb.position = track3_start_point.position;
+            rb.rotation = track3_start_point.rotation;
+        }
+        if (GlobalCycleTrack.TrackType == 4)
+        {
+            rb.position = track4_start_point.position;
+            rb.rotation = track4_start_point.rotation;
+        }
+        if (GlobalCycleTrack.TrackType == 5)
+        {
+            rb.position = track5_start_point.position;
+            rb.rotation = track5_start_point.rotation;
+        }
     }
 }
